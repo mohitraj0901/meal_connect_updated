2edc7df02c40d1833f95f8479a7215ceec5ff7f8
Mohit Raj <165392546+mohitraj0901@users.noreply.github.com>
2026-03-02 23:05:17 +0530

    Updated cors

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors(
-  {
-    origin: 'http://localhost:3000'
-  }
-));
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://mealconnectproject.vercel.app'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 // Connect to MongoDB
