3889e9331395116bbd5862d8f1561bfd4c7f9886
Mohit Raj <165392546+mohitraj0901@users.noreply.github.com>
2026-03-02 23:24:27 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'https://mealconnectproject.vercel.app'
-  ],
+  origin: true,
+  credentials: true
+}));
   credentials: true
 }));
 app.use(express.json());
